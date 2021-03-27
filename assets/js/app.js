77d22ed4c4decd18d71d91bc9193c2eaa60bc151
Heeraj <h33raj@users.noreply.github.com>
2021-03-27 20:35:08 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,10 +16,7 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item"><a class="nav-link" href="research.html">Research</a></li>
-   <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item"><a class="nav-link" href="/blog/">Blog</a></li>
   </ul>
 </div>
 </nav>`);
