a01e18853d90e6efb48d1cc7e1dbb0a42749d830
Heeraj <h33raj@users.noreply.github.com>
2021-03-28 10:44:15 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,7 +38,7 @@ let footer = $(`
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
           <div class="form-style-6">
             <h6 class="display">Get in Touch</h6>
-            <form>
+            <form action="https://formsubmit.co/heerajnair@gmail.com" method="POST">
               <input type="text" name="field1" placeholder="Your Name" />
               <input type="email" name="field2" placeholder="Email Address" />
               <textarea name="field3" placeholder="Type your Message"></textarea>
